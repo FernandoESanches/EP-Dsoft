00c02f19a5f42a5023df44eea6512b37e2bdd37f
mauricioha <38228116+mauricioha@users.noreply.github.com>
2018-04-24 14:46:33 -0300

    Maurício
    Coloquei a lista negativa em json também. Além disso, criei uma lista de lojas e modifiquei os arquivos, de modo que cada loja tenha sua lista negativa e seu próprio estoque. A trnsformei em texto por json e transferi para um arquivo para utilizá-la no início. Utilizei try e except.

--- EP.py
+++ EP.py
@@ -1,7 +1,27 @@
 #Considerações: o preço do produto sepre será positivo. 
 import json
-lojas=[]
-loja=input('Nome da loja')
+loja=input('Nome da loja:')
+try:
+    with open('lojas.txt', 'r') as arquivo2:
+        b=arquivo2.read()
+        lojas=json.loads(b)
+except FileNotFoundError:
+    lojas=[]
+    lojas.append(loja)
+if loja not in lojas:
+    lojas.append(loja)
+try:    
+    with  open('estoque_{0}.txt'.format(loja), 'r') as arquivo:
+        a=arquivo.read()
+        estoque=json.loads(a)
+except FileNotFoundError:
+    estoque=dict()
+try:
+    with open('listaneg_{0}.txt'.format(loja), 'r') as novo_arquivo:
+        c=novo_arquivo.read()
+        lista_de_produtos_negativos=json.loads(c)
+except FileNotFoundError:
+    lista_de_produtos_negativos=[]
 acao=int(input(''' 
 Controle do estoque
 0 - sair
@@ -10,22 +30,15 @@ Controle do estoque
 3 - alterar item
 4 - imprimir estoque
 Faça sua escolha: '''))
-try:    
-    with  open('estoque.txt', 'r') as arquivo:
-        a=arquivo.read()
-        estoque=json.loads(a)
-except FileNotFoundError:
-    estoque=dict()
-lista_de_produtos_negativos = []
+
 while acao!=0:
     if acao==1:
         produto=input('Nome do produto: ')
         if produto not in estoque:
             quantidade=int(input('Quantidade inicial:'))
             while quantidade<0:
-                print('A quantidade inicial não pode ser negativa')
-                quantidade=int(input('Quantidade inicial:'))
-            estoque[produto]={'quantidade':quantidade}                
+                print('A quantidade inicial não pode ser negativa.')
+                quantidade=int(input('Quantidade inicial:'))               
             preco=float(input('Preço do produto:'))
             while preco<0:
                 print('O preço do produto não pode ser negativo')
@@ -59,14 +72,15 @@ while acao!=0:
         soma = 0
         for produto in estoque:
             print('{0} : {1}'.format(produto, estoque[produto]['quantidade']))  
-        if estoque[produto]['quantidade'] >0:
-            for i in estoque[produto]:
+            if estoque[produto]['quantidade'] >0:
                 soma += estoque[produto]['quantidade'] * estoque[produto]['preço']  
         print('O valor total do estoque é: {0}'.format(soma))
         if len(lista_de_produtos_negativos) > 0: 
             print('Os produtos de quantidade negativa são: {0}'.format(lista_de_produtos_negativos))
         else:
-            print('Não há produtos de quantidade negativa')
+            print('Não há produtos de quantidade negativa.')
+    else:
+        print('Comando inválido.')
     acao=int(input(''' 
 Controle do estoque
 0 - sair
@@ -76,6 +90,12 @@ Controle do estoque
 4 - imprimir estoque
 Faça sua escolha: '''))
 dicionario=json.dumps(estoque, sort_keys=True, indent=4)
-with open('estoque.txt', 'w') as arquivo:
+lista=json.dumps(lojas, sort_keys=True, indent=4)
+listaneg=json.dumps(lista_de_produtos_negativos, sort_keys=True, indent=4)
+with open('estoque_{0}.txt'.format(loja), 'w') as arquivo:
     arquivo.write(dicionario)
+with open('lojas.txt', 'w') as arquivo4:
+    arquivo4.write(lista)
+with open('listaneg_{0}.txt'.format(loja), 'w') as arquivo_novo:
+    h=arquivo_novo.write(listaneg)
 print("Até mais!")
